8e01630eaa767ee0f63b7537355d2c50ab7d96c1
sharmachait <55458991+sharmachait@users.noreply.github.com>
2025-08-29 17:12:02 +0530

    Update var.sql

--- var.sql
+++ var.sql
@@ -1,5 +1,8 @@
 DO $$
 DECLARE
-    database_name text := coalesce(current_setting('myapp.databasename', true), 'default_database');
-    server_name text := coalesce(current_setting('myapp.servername', true), 'default_server');
-    env text := coalesce(current_setting('myapp.env', true), 'default_env');
+    database_name text := coalesce(current_setting('map.databasename', true), 'default_database');
+    server_name text := coalesce(current_setting('map.servername', true), 'default_server');
+    env text := coalesce(current_setting('map.env', true), 'default_env');
+
+
+---- psql -c "SET myapp.databasename = '$databasename';SET myapp.servername = '$servername';SET myapp.env = '$env';" -f Postgres.sql
